fca8511d04bf0169c1dd003b4822604d97893f00
unknown <sunebarikara@gmail.com>
2024-12-25 18:23:29 +0100

    Second Commit

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/Kara.tsx
+              app/kara.tsx
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly at app/kara.tsx.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
